42127953a46fd6e7c195cd5a8773b7df6a733537
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-10-14 13:44:05 +0200

    add nix flake file

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Umons LaTeX";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    with flake-utils.lib; eachSystem allSystems (system:
+      let
+        umons-latex = pkgs.stdenv.mkDerivation {
+            name = "umons-latex";
+            pname = "umons-latex";
+            src = self;
+
+            dontBuild = true;
+
+            installPhase = ''
+                runHook preInstall
+
+                mkdir -p $out/tex/latex
+                cp -ar $src $out/tex/latex/umons-latex
+
+                runHook postInstall
+            '';
+
+            tlType = "run";
+        };
+      in
+      {
+        # Nix shell / nix build
+        packages.default = umons-latex;
+      });
+}
