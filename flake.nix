1638dd1bc208e5c8ca3fb04b0fd339c61df6bc3d
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-03-07 07:33:33 +0100

    refactor: switch to `flake-parts`

--- flake.nix
+++ flake.nix
@@ -3,71 +3,76 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
     nix-filter.url = github:numtide/nix-filter;
   };
 
-  outputs = { self, nixpkgs, flake-utils, nix-filter, ... }@inputs:
-    let
-      overlay-latex-umons = nixpkgs: final: prev: {
-        latex-umons = prev.stdenvNoCC.mkDerivation {
-          name = "latex-umons";
-          pname = "latex-umons";
-          src = nix-filter.lib.filter {
-            root = ./.;
-            exclude = [ ./template ];
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = inputs.nixpkgs.lib.systems.flakeExposed;
+      flake = let
+        overlay-latex-umons = nixpkgs: final: prev: {
+          latex-umons = prev.stdenvNoCC.mkDerivation {
+            name = "latex-umons";
+            pname = "latex-umons";
+            src = inputs.nix-filter.lib.filter {
+              root = ./.;
+              exclude = [./template];
+            };
+
+            dontBuild = true;
+
+            installPhase = ''
+              runHook preInstall
+
+              mkdir -p $out/tex/latex/umons
+              cp -ar $src/src/latex/* $out/tex/latex/umons
+
+              runHook postInstall
+            '';
+
+            tlType = "run";
           };
+          pandoc-template-umons = prev.stdenvNoCC.mkDerivation {
+            name = "pandoc-template-umons";
+            pname = "pandoc-template-umons";
+            src = inputs.nix-filter.lib.filter {
+              root = ./.;
+              exclude = [./template];
+            };
 
-          dontBuild = true;
+            dontBuild = true;
 
-          installPhase = ''
-            runHook preInstall
+            installPhase = ''
+              runHook preInstall
 
-            mkdir -p $out/tex/latex/umons
-            cp -ar $src/src/latex/* $out/tex/latex/umons
+              mkdir -p $out
+              cp -ar $src/src/pandoc/* $out/
 
-            runHook postInstall
-          '';
-
-          tlType = "run";
+              runHook postInstall
+            '';
+          };
         };
-        pandoc-template-umons = prev.stdenvNoCC.mkDerivation {
-          name = "pandoc-template-umons";
-          pname = "pandoc-template-umons";
-          src = nix-filter.lib.filter {
-            root = ./.;
-            exclude = [ ./template ];
-          };
+      in {
+        overlays.default = overlay-latex-umons inputs.nixpkgs;
 
-          dontBuild = true;
-
-          installPhase = ''
-            runHook preInstall
-
-            mkdir -p $out
-            cp -ar $src/src/pandoc/* $out/
-
-            runHook postInstall
-          '';
-
-          tlType = "run";
+        # nix flake new --template templates#default ./my-new-document
+        templates.default = {
+          path = ./template;
+          description = "A template for creating PDF document with UMons theme with Pandoc or LaTeX.";
         };
       };
-    in
-    {
-      overlays.default = overlay-latex-umons nixpkgs;
 
-      # nix flake new --template templates#default ./my-new-document
-      templates.default = {
-        path = ./template;
-        description = "A template for creating PDF document with UMons theme with Pandoc or LaTeX.";
-      };
-    } //
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: let
+        pkgs = import inputs.nixpkgs {
           overlays = [
-            self.overlays.default
+            inputs.self.overlays.default
           ];
           inherit system;
         };
@@ -77,10 +82,15 @@
         '';
 
         tex = pkgs.texlive.combine {
-          inherit (pkgs.texlive) scheme-full latex-bin latexmk;
+          inherit (pkgs.texlive)
+            scheme-full
+            latex-bin
+            latexmk
+            lettrine
+            ;
 
           latex-umons = {
-            pkgs = [ pkgs.latex-umons ];
+            pkgs = [pkgs.latex-umons];
           };
         };
 
@@ -91,32 +101,36 @@
           "presentation"
         ];
 
-        documentTypes = nixpkgs.lib.genAttrs documentNames (
+        documentTypes = inputs.nixpkgs.lib.genAttrs documentNames (
           name:
-          pkgs.stdenvNoCC.mkDerivation {
-            name = "umons-" + name;
+            pkgs.stdenvNoCC.mkDerivation {
+              name = "umons-" + name;
 
-            src = pkgs.lib.cleanSource ./.;
+              src = pkgs.lib.cleanSource ./.;
 
-            buildInputs = [
-              tex
-              pandoc
-              pkgs.gnumake
-            ];
+              buildInputs = [
+                tex
+                pandoc
+                pkgs.gnumake
+              ];
 
-            buildPhase = ''
-              make -C template build-${name}
-            '';
+              buildPhase = ''
+                runHook preBuild
 
-            installPhase = ''
-              runHook preInstall
+                make -C template build-${name}
 
-              mkdir -p $out
-              cp template/${name}.pdf $out/
+                runHook postBuild
+              '';
 
-              runHook postInstall
-            '';
-          }
+              installPhase = ''
+                runHook preInstall
+
+                mkdir -p $out
+                cp template/${name}.pdf $out/
+
+                runHook postInstall
+              '';
+            }
         );
 
         pandoc-exercice-umons = pkgs.writeShellApplication {
@@ -124,15 +138,18 @@
           text = ''
             ${pkgs.pandoc}/bin/pandoc --from markdown --to latex -s --template=${pkgs.pandoc-template-umons}/templates/umons.latex -o pandoc-exercice-umons.pdf "$@"
           '';
-          runtimeInputs = [ tex ];
+          runtimeInputs = [tex];
         };
-      in
-      {
+      in {
+        formatter = pkgs.alejandra;
+
         # Nix shell / nix build
-        packages = documentTypes // {
-          default = pkgs.latex-umons;
-          pandoc-exercice-umons = pandoc-exercice-umons;
-        };
+        packages =
+          documentTypes
+          // {
+            default = pkgs.latex-umons;
+            pandoc-exercice-umons = pandoc-exercice-umons;
+          };
 
         # Nix develop
         devShells.default = pkgs.mkShellNoCC {
@@ -140,11 +157,10 @@
           buildInputs = [
             tex
             pandoc
-            pkgs.nixpkgs-fmt
-            pkgs.nixfmt
           ];
         };
 
         checks = documentTypes;
-      });
+      };
+    };
 }
